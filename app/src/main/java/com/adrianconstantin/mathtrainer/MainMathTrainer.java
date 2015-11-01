98aecd55ac22d50efae5dce706ce4a45ac9864c0
Adrian Constantin Vaideanu <Adrian Constantin Vaideanu>
2015-11-01 18:30:43 +0200

    Delete the menu and mail button from default theme.

--- app/src/main/java/com/adrianconstantin/mathtrainer/MainMathTrainer.java
+++ app/src/main/java/com/adrianconstantin/mathtrainer/MainMathTrainer.java
@@ -17,21 +17,10 @@ public class MainMathTrainer extends AppCompatActivity {
         setContentView(R.layout.activity_main_math_trainer);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main_math_trainer, menu);
         return true;
     }
 
@@ -41,11 +30,6 @@ public class MainMathTrainer extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
         return super.onOptionsItemSelected(item);
     }
