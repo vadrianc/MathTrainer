07ecb81b5b11a0a33cdbf1e91794bf08090c7d12
Adrian Constantin Vaideanu <Adrian Constantin Vaideanu>
2015-11-05 00:34:31 +0200

    Add listener for text change and random generator.

--- app/src/main/java/com/adrianconstantin/mathtrainer/activity/OperationHandlerActivity.java
+++ app/src/main/java/com/adrianconstantin/mathtrainer/activity/OperationHandlerActivity.java
@@ -6,15 +6,23 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.adrianconstantin.mathtrainer.R;
 import com.adrianconstantin.mathtrainer.base.IOperationHandler;
+import com.adrianconstantin.mathtrainer.base.IRandomGenerator;
 import com.adrianconstantin.mathtrainer.exception.UnknownOperationException;
+import com.adrianconstantin.mathtrainer.impl.IntegerRandomGenerator;
 import com.adrianconstantin.mathtrainer.utils.Utils;
 
 public class OperationHandlerActivity extends AppCompatActivity {
+
+    IOperationHandler mOperationHandler = null;
+    IRandomGenerator<Integer> mRandomGenerator = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,27 +32,62 @@ public class OperationHandlerActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        init();
         try {
             handleIntent();
         } catch (UnknownOperationException e) {
             e.printStackTrace();
         }
+
+        populateTextView();
+    }
+
+    private void init()
+    {
+        mRandomGenerator = new IntegerRandomGenerator(100);
+
+        EditText resultEditText = (EditText)findViewById(R.id.resultEditText);
+        resultEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // No implementation.
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // No implementation.
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
 
     private IOperationHandler handleIntent() throws UnknownOperationException {
-        IOperationHandler operationHandler = null;
-
         Intent intent = getIntent();
-        operationHandler  = intent.getExtras().getParcelable(Utils.OPERATION);
+        mOperationHandler  = intent.getExtras().getParcelable(Utils.OPERATION);
 
-        if (operationHandler == null)
+        if (mOperationHandler == null)
         {
             throw new UnknownOperationException();
         }
 
-        TextView charTextView = (TextView)findViewById(R.id.char_text);
-        charTextView.setText(Character.toString(operationHandler.GetOperationSymbol()));
-
-        return operationHandler;
+        return mOperationHandler;
     }
+
+    private void generateOperands() {
+        mOperationHandler.SetFirstOperand(mRandomGenerator.Generate().doubleValue());
+        mOperationHandler.SetSecondOperand(mRandomGenerator.Generate().doubleValue());
+    }
+
+    private void populateTextView(){
+        generateOperands();
+
+        TextView charTextView = (TextView)findViewById(R.id.char_text);
+        charTextView.setText(mOperationHandler.GetExpression());
+    }
+
 }
