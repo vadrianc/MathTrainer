273b7020aa183dfd8cf51b494de8d14fcd873408
Adrian Constantin Vaideanu <Adrian Constantin Vaideanu>
2015-11-07 18:09:29 +0200

    Added method to calculate the possible maximum value for an operand.

--- app/src/main/java/com/adrianconstantin/mathtrainer/utils/Utils.java
+++ app/src/main/java/com/adrianconstantin/mathtrainer/utils/Utils.java
@@ -5,7 +5,27 @@ package com.adrianconstantin.mathtrainer.utils;
  */
 public class Utils {
     /**
+     *
+     */
+    private static final char BASE_FOR_MAX = '9';
+
+    /**
      * Operation type constant.
      */
     public static final String OPERATION = "Operation";
+
+    /**
+     *
+     * @param length
+     * @return
+     */
+    public static Integer GetMaximumInteger(int length){
+        StringBuilder maxBuilder = new StringBuilder();
+
+        for (int index = 0; index < length; index++){
+            maxBuilder.append(BASE_FOR_MAX);
+        }
+
+        return Integer.parseInt(maxBuilder.toString()) + 1;
+    }
 }
